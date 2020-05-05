3998130715a5f47bc3eb6c95c5216877d7a70636
nate <mtr574@gmail.com>
2020-05-05 17:51:01 +0300

    -

--- src/components/SellingCard/index.js
+++ src/components/SellingCard/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col, Input, Button } from "reactstrap";
 import "./style.scss";
 
 const SellingCard = ({ price, listPrice }) => {
@@ -22,14 +23,25 @@ const SellingCard = ({ price, listPrice }) => {
 
       <div className="list-price">List Price: ${listPrice}</div>
 
-      <div>
-        <input value="$260,200" />
+      <Input value="$260,200" className="price-desktop" />
+
+      <div className="controls-desktop">
+        <Button className="btn-review btn-primary">Review Bid</Button>
+        <button className="btn-buy-now">Buy It Now</button>
       </div>
 
-      <div className="controls">
-        <button className="btn-primary">Review Bid</button>
-        <button>Buy It Now</button>
-      </div>
+      <Row className="price-and-controls-mobile">
+        <Col xs="6">
+          <Input value="$260,200" />
+        </Col>
+
+        <Col xs="6">
+          <div className="controls">
+            <Button className="btn-review btn-primary">Review Bid</Button>
+            <button className="btn-buy-now">Buy It Now</button>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
